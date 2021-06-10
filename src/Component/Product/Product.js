600c2c82e56b6c7a158ef1c8e7327763d030006b
SaultA <alyssa.sault131@gmail.com>
2021-06-10 11:19:49 +0200

    deuxieme commit

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import "./Product.css"
-import { Card } from 'antd';
+import { Card, Button } from 'antd';
+import Axios from 'axios'; 
 
 export default class Product extends React.Component{
 
 constructor(props){
 super(props) ;// appel du constructeur de react component
+this.state = {
+    products : []
+
+}
+}
+
+//  des que le composant se charge tu fais ça: 
+componentDidMount(){
+    this.getProduct()
+
+}
+
+//  Affiher la liste des produits Get
+
+getProduct(){
+    const axios = require('axios');
+
+// Make a request for a user with a given ID
+Axios.get('http://localhost:49964/api/Apiproduits')
+  .then( (response)=> {
+this.setState({
+    products: response.data
+})
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 }
 
 render(){
@@ -16,11 +48,15 @@ render(){
             <>
                 <h1> Liste des produits:</h1>
 
-                <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                </Card>
+                {this.state.products.map((product, i) => {
+                    return (
+                        <Card title={product.nameproduct} style={{ width: 300 }}>
+                            <p>price : {product.priceProduct}</p>
+                            <p>id : {product.id}</p>
+    
+                        </Card>)
+                })}
+
             </>
             )
         }
